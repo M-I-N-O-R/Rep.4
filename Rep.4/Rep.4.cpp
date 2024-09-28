f941b06912887dcb33d2588aa05cf72e7fc8a1a3
Олександр <goncars451@gmail.com>
2024-09-28 14:15:05 +0300

    Добавьте файлы проекта.

---
+++ Rep.4/Rep.4.cpp
@@ -0,0 +1,24 @@
+﻿#include <iostream>
+using namespace std;
+int main()
+{
+    setlocale(0, ".1251");
+    int age;
+    int salary;
+    string name;
+    cout << "Введiть свiй вiк: ";
+    cin >> age;
+    cout << "Введiть свою зарплату: ";
+    cin >> salary;
+    if (age > 20 && salary > 400 && salary < 1000) {
+        cout << "Введiть своє iм'я: ";
+        cout << "Введiть своє прiзвище: ";
+        cout << "Введiть бажану посаду: ";
+        cin >> name;
+        cout << " Вiтаємо, ми розглянемо вашу позицiю " << name << "." << endl;
+
+    }
+    else {
+        cout << "Вибачте, ви не пiдходите" << endl;
+    }
+}
